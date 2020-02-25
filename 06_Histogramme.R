9cea77c3ce0109d208e48fcfe5d33fb2b0456de6
MattSen <49534973+MattSen@users.noreply.github.com>
2020-02-25 13:14:21 +0100

    Update 06_Histogramme.R

--- 06_Histogramme.R
+++ 06_Histogramme.R
@@ -25,17 +25,13 @@ hist(iris$Sepal.Width,
      las=1,
      xlim=c(0,4.5),
      ylim=c(0,40),
-     xlab="Sepal Width",
-     col=viridis(2,alpha=0.5)[2])
+     xlab="Width",
+     col=viridis(3,alpha=0.5)[3])
 
 hist(iris$Petal.Width,
      col=viridis(3,alpha=0.5)[2],
      add=TRUE)
 
 legend("topright",legend=c("Sepalen","Petalen"),pch=15,
-       col=c(viridis(2,alpha=0.5)[2],viridis(3,alpha=0.5)[2]),
+       col=c(viridis(3,alpha=0.5)[3],viridis(3,alpha=0.5)[2]),
        bty="n")
-
-
-
-
